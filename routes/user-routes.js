d8fc38e22b537df9720f05a26a1fd2cc49883826
Jason Rowland <jasonrowland86>
2017-08-21 17:31:10 -0400

    Created user-routes.js

---
+++ routes/user-routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const userRoutes = express.Router();
+
+const usersController = require('../controllers/movies-controller');
+
+
+userRoutes.get('/', usersController.index);
+userRoutes.post('/', usersController.create);
+userRoutes.get('/:id', usersController.show);
+userRoutes.put('/:id', usersController.update);
+
+module.exports = userRoutes;
