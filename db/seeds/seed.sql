9ada698c5bc4ce822be5abee79d208df8afbb01b
Rajiste Bennin <rajisteb@gmail.com>
2017-08-21 16:48:39 -0400

    db redo and seed

--- db/seeds/seed.sql
+++ db/seeds/seed.sql
@@ -1,15 +1,15 @@
 \c simplemgmt_dev;
 
 INSERT INTO projects(name, description, category, status, planned_start_date, planned_end_date) 
-VALUES ('MVP - ToDo', 'Create minimum viable product todo application', 'Tech', 0, '2017-04-09', '2017-25-09');
+VALUES ('MVP - ToDo', 'Create minimum viable product todo application', 'Tech', 0, '2017-09-04', '2017-09-25');
 INSERT INTO projects(name, description, category, status, planned_start_date, planned_end_date) 
-VALUES ('Wireframe - UI', 'Create inital wireframes and UI templates', 'Design', 0, '2017-04-09', '2017-27-09');
+VALUES ('Wireframe - UI', 'Create inital wireframes and UI templates', 'Design', 0, '2017-09-04', '2017-09-27');
 INSERT INTO projects(name, description, category, status, planned_start_date, planned_end_date) 
-VALUES ('Conference Prep', 'Prepare materials for developer conference', 'Administration', 0, '2017-04-09', '2017-25-09');
+VALUES ('Conference Prep', 'Prepare materials for developer conference', 'Administration', 0, '2017-09-04', '2017-09-25');
 INSERT INTO projects(name, description, category, status, planned_start_date, planned_end_date) 
-VALUES ('React refactor', 'Refactor express views into React using react routes', 'Tech', 0, '2017-02-10', '2017-06-10');
+VALUES ('React refactor', 'Refactor express views into React using react routes', 'Tech', 0, '2017-10-02', '2017-10-06');
 INSERT INTO projects(name, description, category, status, planned_start_date, planned_end_date) 
-VALUES ('Interactive Layout', 'Create interative UI app layout', 'Design', 0, '2017-03-10', '2017-09-10');
+VALUES ('Interactive Layout', 'Create interative UI app layout', 'Design', 0, '2017-10-03', '2017-10-09');
 INSERT INTO projects(name, description, category, status, planned_start_date, planned_end_date) 
-VALUES ('Office Management', 'Prepare for dept meetings and new employee orientation', 'Administration', 0, '2017-11-09', '2017-15-09');
+VALUES ('Office Management', 'Prepare for dept meetings and new employee orientation', 'Administration', 0, '2017-09-11', '2017-09-18');
 
