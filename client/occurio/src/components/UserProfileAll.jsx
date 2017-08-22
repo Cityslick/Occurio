7562b25a4f8bbb806c1dd0e0103486a7e5234712
Rajiste Bennin <rajisteb@gmail.com>
2017-08-22 11:36:54 -0400

    config userall comp

--- client/occurio/src/components/UserProfileAll.jsx
+++ client/occurio/src/components/UserProfileAll.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const UserProfileAll = () => {
+const UserProfileAll = (props) => {
+
+  axios.get('/users', { //refactor to actual route
+    username,
+    firstname,
+    lastname,
+    img_url,
+  }).then(res => {
+    console.log(res); //logging response
+    this.setState({
+      userData: res.data.user
+    })
+  })
+
+
     return (
-        <div>
-          <div>
-            <div>Thomas</div>
-            <div>Aury</div>
-            <div>Raj</div>
-            <div>Jason</div>
-            <div>Ramsey</div>
-          </div>
+        <div className='userList'>
+          <select>
+            {props.userData.map(users => {
+              return <option>{users.img_url}{users.username}{users.name}</option>
+            })}
+          </select>
         </div>
     )
 }
