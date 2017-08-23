0e5749e10f2dbac29e2ff78cc1085a5e6d71aca1
Aury Rodriguez <arm010286@hotmail.com>
2017-08-23 12:31:21 -0400

    adding filters

--- client/occurio/src/components/Register.jsx
+++ client/occurio/src/components/Register.jsx
@@ -14,7 +14,7 @@ class Register extends Component {
             proj_link: '',
             user_type: '',
         }
-        this.handleInputChange = this.handleInputChange.bind(this);   
+        this.handleInputChange = this.handleInputChange.bind(this);
     }
 
     handleInputChange(e) {
@@ -34,7 +34,7 @@ class Register extends Component {
                 </h1>
 
                   <form onSubmit={(e) => this.props.handleRegisterSubmit(
-                    e, 
+                    e,
                     this.state.username,
                     this.state.firstname,
                     this.state.lastname,
@@ -45,34 +45,50 @@ class Register extends Component {
                     this.state.user_type
                     )}>
                     <div>
-                    <input className="form" type="text" name="username" value={this.state.username} placeholder="Username" onChange={this.handleInputChange} />
+                        <input className="form" type="text" name="username" value={this.state.username} placeholder="Username" onChange={this.handleInputChange} />
                     </div>
+
                     <div>
-                    <input className="form" type="text" name="firstname" value={this.state.firstname} placeholder="First Name" onChange={this.handleInputChange} />
+                        <input className="form" type="text" name="firstname" value={this.state.firstname} placeholder="First Name" onChange={this.handleInputChange} />
                     </div>
+
                     <div>
-                    <input className="form" type="text" name="lastname" value={this.state.lastname} placeholder="Last Name" onChange={this.handleInputChange} />
+                        <input className="form" type="text" name="lastname" value={this.state.lastname} placeholder="Last Name" onChange={this.handleInputChange} />
                     </div>
+
                     <div>
-                    <input className="form" type="text" name="password" value={this.state.password} placeholder="Password" onChange={this.handleInputChange} />
+                        <input className="form" type="password" name="password" value={this.state.password} placeholder="Password" onChange={this.handleInputChange} />
                     </div>
+
                     <div>
-                    <input className="form" type="text" name="email" value={this.state.email} placeholder="Email Address" onChange={this.handleInputChange} />
+                        <input className="form" type="text" name="email" value={this.state.email} placeholder="Email Address" onChange={this.handleInputChange} />
                     </div>
+
                     <div>
-                    <input className="form" type="text" name="img_url" value={this.state.img_url} placeholder="Image URL" onChange={this.handleInputChange} />
+                        <input className="form" type="text" name="img_url" value={this.state.img_url} placeholder="Image URL" onChange={this.handleInputChange} />
                     </div>
+
                     <div>
-                    <input className="form" type="text" name="proj_link" value={this.state.proj_link} placeholder="Link to Project" onChange={this.handleInputChange} />
+                        <input className="form" type="text" name="proj_link" value={this.state.proj_link} placeholder="Link to Project" onChange={this.handleInputChange} />
                     </div>
+
                     <div>
-                    <input className="form" type="text" name="user_type" value={this.state.user_type} placeholder="User Type" onChange={this.handleInputChange} />
+                        <select className="dropDown">
+                            <option>Manager</option>
+                            <option>Collaborator</option>
+                            <option>Something Else</option>
+                        </select>
+
+
+                        <input className="form" type="text" name="user_type" value={this.state.user_type} placeholder="User Type" onChange={this.handleInputChange} />
                     </div>
+
                     <div>
-                    <input className="form" type="submit" value="Enter" />
+                        <input className="form" type="submit" value="Enter" />
                     </div>
+
                     <div>
-                    <h1>Okurio is the easiest way to manage.</h1>
+                        <h1>Okurio is the easiest way to manage.</h1>
                     </div>
                   </form>
               </div>
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -6,8 +6,8 @@ const taskController= require("../controllers/taskController");
 taskRoutes.post("/",taskController.assignTasks )
 taskRoutes.put("/:task_id",taskController.updateTasks )
 taskRoutes.delete("/:task_id",taskController.deleteTask);
-taskRoutes.get("/:proj_id",taskController.findProjectTasks);
-taskRoutes.post("/:user_id",taskController.findCollaboratorsTasks);
+taskRoutes.post("/:proj_id",taskController.findProjectTasks);
+//taskRoutes.post("/:user_id",taskController.findCollaboratorsTasks);
 
 
 module.exports= taskRoutes;
