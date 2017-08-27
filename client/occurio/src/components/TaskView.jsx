9401df2ce9923b6bc2472d99427cafffdc0d5b1f
Rajiste Bennin <rajisteb@gmail.com>
2017-08-27 19:17:43 -0400

    style task list

--- client/occurio/src/components/TaskView.jsx
+++ client/occurio/src/components/TaskView.jsx
@@ -3,15 +3,15 @@ import '../App.css';
 
 const TaskView = (props) => {
   return (
-    <tr>
-      <td>{props.index}</td>
-      <td>{props.task.description}</td>
-      <td>{props.task.start_datestr}</td>
-      <td>{props.task.end_datestr}</td>
-      <td>{props.task.ticket}</td>
-      <td>{props.task.status}</td>
-      <td>{props.task.fullname}</td>
-    </tr>
+    <div className="tasks-table">
+      <div className="table-data-num">{props.index}</div>
+      <div className="table-data-desc">{props.task.description}</div>
+      <div className="table-data-date">{props.task.start_datestr}</div>
+      <div className="table-data-date">{props.task.end_datestr}</div>
+      <div className="table-data">{props.task.ticket}</div>
+      <div className="table-data-status">{props.task.status}</div>
+      <div className="table-data-name">{props.task.fullname}</div>
+    </div>
   )
 }
 
